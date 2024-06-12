13892de121ed7254d8e50ac6885c5b167ee6a421
anisanhy <joyabrianrantepasang14@gmail.com>
2024-06-12 21:08:33 +0800

    feat: create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './pages/landing_page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/landing_page.jsx
@@ -0,0 +1,9 @@
+import LandingPageHero from "../components/Fragments/Landing_Page/landing_page_hero";
+
+const LandingPage = () => {
+    return (
+        <LandingPageHero />
+    );
+}
+
+export default LandingPage;
