00f15125e8150058ce4a0d4822f661bae59bdfee
MilhamMaulanaR <muh.ilhammaulana29@gmail.com>
2024-06-24 16:55:39 +0800

    hubungkan frontend dan backEnd untuk dashboard admin

--- src/components/Fragments/Dashboard/dashboard_users.jsx
+++ src/components/Fragments/Dashboard/dashboard_users.jsx
@@ -1,55 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const DashboardUser = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/users')
+        .then(response => {
+            setUsers(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching users:', error);
+        });
+    }, []);
+
     return (
         <div className="mt-5 mx-10 font-Inter font-medium">
-            <div className="ml-4">
-                <h1 className="text-2xl font-bold">Users</h1>
-            </div>
-            <div className="mt-8 mx-12">
-                <table className="w-full">
-                    <thead className="border-b-[1px] border-gray-800">
-                        <tr>
-                            <th className="text-left font-medium py-4">ID</th>
-                            <th className="text-left font-medium py-4">NAMA</th>
-                            <th className="text-left font-medium py-4">PHONE NUMBER</th>
-                            <th className="text-left font-medium py-4">E-MAIL</th>
-                        </tr>
-                    </thead>
-                    <tbody className="mt-4">
-                        <tr className="border-b-[0.5px] border-gray-200">
-                            <td className="py-2 pt-4">1</td>
-                            <td className="py-2 pt-4">Joy</td>
-                            <td className="py-2 pt-4">08123456789</td>
-                            <td className="py-2 pt-4">example@gmail.com</td>
-                        </tr>
-                        <tr className="border-b-[0.5px] border-gray-200">
-                            <td className="py-2 pt-4">2</td>
-                            <td className="py-2 pt-4">Joy</td>
-                            <td className="py-2 pt-4">08123456789</td>
-                            <td className="py-2 pt-4">example@gmail.com</td>
-                        </tr>
-                        <tr className="border-b-[0.5px] border-gray-200">
-                            <td className="py-2 pt-4">3</td>
-                            <td className="py-2 pt-4">Joy</td>
-                            <td className="py-2 pt-4">08123456789</td>
-                            <td className="py-2 pt-4">example@gmail.com</td>
-                        </tr>
-                        <tr className="border-b-[0.5px] border-gray-200">
-                            <td className="py-2 pt-4">4</td>
-                            <td className="py-2 pt-4">Joy</td>
-                            <td className="py-2 pt-4">08123456789</td>
-                            <td className="py-2 pt-4">example@gmail.com</td>
-                        </tr>
-                        <tr className="border-b-[0.5px] border-gray-200">
-                            <td className="py-2 pt-4">5</td>
-                            <td className="py-2 pt-4">Joy</td>
-                            <td className="py-2 pt-4">08123456789</td>
-                            <td className="py-2 pt-4">example@gmail.com</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+        <div className="ml-4">
+            <h1 className="text-2xl font-bold">Users</h1>
         </div>
-    )
-}
+        <div className="mt-8 mx-12">
+            <table className="w-full">
+            <thead className="border-b-[1px] border-gray-800">
+                <tr>
+                <th className="text-left font-medium py-4">ID</th>
+                <th className="text-left font-medium py-4">Nama</th>
+                <th className="text-left font-medium py-4">Phone Number</th>
+                <th className="text-left font-medium py-4">Email</th>
+                </tr>
+            </thead>
+            <tbody className="mt-4">
+                {users.map(user => (
+                <tr key={user.id} className="border-b-[0.5px] border-gray-200">
+                    <td className="py-2 pt-4">{user.id}</td>
+                    <td className="py-2 pt-4">{user.username}</td>
+                    <td className="py-2 pt-4">{user.phone_number}</td>
+                    <td className="py-2 pt-4">{user.email}</td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+        </div>
+    );
+};
 
 export default DashboardUser;
