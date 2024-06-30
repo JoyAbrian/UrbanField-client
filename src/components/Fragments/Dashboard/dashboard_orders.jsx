0eedb2505f3030e3aed05de7c11b4c8c3050aa3e
anisanhy <joyabrianrantepasang14@gmail.com>
2024-06-30 09:37:50 +0800

    fix: fix date type

--- src/components/Fragments/Dashboard/dashboard_orders.jsx
+++ src/components/Fragments/Dashboard/dashboard_orders.jsx
@@ -16,6 +16,15 @@ const DashboardOrder = () => {
         });
     }, []);
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        
+        return `${year}-${month}-${day}`;
+    }
+
     return (
         <div className="mt-5 mx-10 font-Inter font-medium">
             <div className="ml-4">
@@ -39,7 +48,7 @@ const DashboardOrder = () => {
                                 <td className="py-4">{booking.id}</td>
                                 <td className="py-4">{booking.user_name}</td>
                                 <td className="py-4">{booking.field_name}</td>
-                                <td className="py-4">{booking.date}</td>
+                                <td className="py-4">{formatDate(booking.date)}</td>
                                 <td className="py-4">{booking.time}</td>
                                 <td className="py-4">{booking.payment_method_name}</td>
                             </tr>
